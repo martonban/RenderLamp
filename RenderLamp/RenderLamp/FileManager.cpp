0c0015a498007b791d56ba86b619ccb9003b07c8
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-09-30 15:16:32 +0200

    nuke again

--- RenderLamp/RenderLamp/FileManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "FileManager.h"
-
-// The path need to use '/' or '\\' insted of '\'
-// TODO:  
-bool FileManager::IsThisPathExist(const std::string& name) {
-	struct stat buffer;
-	return (stat(name.c_str(), &buffer) == 0);
-}
--- RenderLamp/RenderLamp/RenderLampManager.cpp
+++
@@ -1,1 +0,0 @@
-#include "RenderLampManager.h"
