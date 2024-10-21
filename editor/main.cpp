bebc4802eb48b0486cb6cabb2761f844d6e4f036
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-10-21 15:07:58 +0200

    backup before the nuke

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,28 +1,4 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
-#include "renderer/renderer.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -51,11 +27,9 @@ int main(void)
         //----------------------------------------------------------------------------------
 
         int a = 200;
-
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            //int a = 20;
 
             DrawText("Teeest", 190, 200, a, LIGHTGRAY);
 
--- renderer/renderer/renderer.h
+++ renderer/renderer/renderer.h
@@ -3,6 +3,7 @@
 
 class Renderer {
 public:
+    Renderer();
     int Test();
 };
 
