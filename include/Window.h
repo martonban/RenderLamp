e634b5cca43d8a8110fd4f53912d863355ac3494
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-10-24 15:55:30 +0200

    Window System init

---
+++ include/Window.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+ *                             RenderLamp - Window.h
+ *                         Copyright (c) Márton Bán 2024
+ *
+ * This class will be the base class for any Window in this application. 
+ * Currently raylib is not supporting a multiple window rendering by 
+ * default. That's why the main design philosophy will be on inharitance. 
+ * If needed this class can be a base class and you can inherited it later.
+ * 
+ * Currently this class inherited once by the editor
+ * 
+ * 
+ * 
+****************************************************************************/
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#endif
