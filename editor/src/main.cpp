cbc57f2c0783e16a7208306f71de749e9fd929bc
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-02-18 21:40:53 +0100

    CMAKE Done

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,5 +1,5 @@
 #include "Application.h"
-// #include <nlohmann/json.hpp>
+
 
 /****************************************************************************
  *                             RenderLamp - main.cpp
--- renderer/include/renderer.h
+++ renderer/include/renderer.h
@@ -1,6 +1,7 @@
 #ifndef RENDERER_H
 #define RENDERER_H 
 
+
 class BuzzRenderer {
 public:
     BuzzRenderer();
