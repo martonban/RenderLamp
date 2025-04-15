e1677d8dd2779385c40e056361e1bd1cfd9e1c87
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-04-15 15:08:30 +0200

    Ray Tracer Init

---
+++ renderer/include/math.hpp
@@ -0,0 +1,11 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+/*
+    This classes based on the https://raytracing.github.io/books/RayTracingInOneWeekend.html
+*/
+
+class vec4 {
+
+};
+
+#endif
---
+++ renderer/include/ray.hpp
@@ -0,0 +1,6 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+/*
+        This classe based on the https://raytracing.github.io/books/RayTracingInOneWeekend.html
+*/
+#endif
