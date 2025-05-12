ab29a3debabd239bf374e811c28a03ea1f5d5004
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-05-12 22:01:18 +0200

    Add Component

---
+++ editor/include/Componenet.hpp
@@ -0,0 +1,14 @@
+#ifndef COMPONENT_HPP
+#define COMPONENT_HPP
+
+class Component {
+    public:
+        virtual void Init();
+        virtual void Update();
+        virtual void Destroy();
+        virtual void Gui();
+        virtual void RenderBatch();
+};
+
+
+#endif
