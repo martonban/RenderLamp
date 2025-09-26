257c73fe032e4e30fea4116017b2beb21ee8b2ca
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-09-26 21:20:47 +0200

    Arca Asset init

---
+++ vendors/Arca/include/ArcaAsset.hpp
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------------------------
+//                                        Arca - ArcaAssets
+//                                       Márton Bán (C) 2025
+//	
+//  ArcaAssets is a file creator tool for different usages.
+//------------------------------------------------------------------------------------------------
+
+#ifndef ARCA_ASSETS_HPP
+#define ARCA_ASSETS_HPP
+
+class ArcaAssets {
+    
+};
+
+#endif 
