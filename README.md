4a62da1b7738016ab4a242b2dc8c7652af9dba14
Márton Bán <30813844+martonban@users.noreply.github.com>
2026-03-07 16:45:57 +0100

    Nuke it again

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 RenderLamp is a DCC tool with a CPU ray tracer engine
 
 # Sources:
+- https://refactoring.guru/design-patterns/singleton/cpp/example
+
+
+
+
 - https://www.youtube.com/watch?v=VyTys5oCdN8
 - https://www.youtube.com/watch?v=elT8_n0_qdc
 - https://www.youtube.com/watch?v=5Hog6a0EYa0
--- application/src/Main.cpp
+++ application/src/Main.cpp
@@ -1,29 +1,8 @@
-#include "interface/Printer.hpp"
 #include "ServerInstance.hpp"
-#include "Arca.hpp"
 
 int main() {
-    // TODO::ARCA INSTACE LATER
-    Arca::InitArcaInstance("BuzzRenderer");
-    if(Arca::FetchArcaInstanceData()) {
-        Arca::BuildArcaInstance();
-        std::cout << "Arca instance has been de-serialized and build!" << std::endl;
-    } else {
-        Arca::CreateArcaInstance();
-        Arca::AddCreator("Márton Bán");
-        Arca::CreateModule("Editor");
-        Arca::CreateModule("Renderer");
-        if(Arca::ReleaseArcaInstance()) {
-            std::cout << "Arca Instance has been created!" << std::endl;
-        }
-    }
-
     // Singleton Application
-    ServerInstance& renderingServerInstance = ServerInstance::GetInstance();
-    renderingServerInstance.Init();
-    renderingServerInstance.Start();
-
-    char c;
-    std::cin >> c;
-    return 0;
+    ServerInstance* serverInstance = ServerInstance::GetInstance();
+    serverInstance -> Init();
+    serverInstance -> Start();
 }
